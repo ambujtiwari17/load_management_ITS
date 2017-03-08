4927ec95be88b47f89e15542a57306eaecfcfbff
ambujtiwari17 <ambuj.t14@iiits.in>
2017-03-08 17:10:28 +0530

    Added install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-get install python python-dev python-pip libpq-dev postgresql postgresql-contrib
+sudo pip install django==1.9
+sudo pip install flask
+sudo pip install flask-mysql
+sudo apt-get install python-mysqldb
+sudo pip install flask-restful
+sudo pip install  flask_sqlalchemy
+
+#MySQL installation.
+
+sudo apt-get install mysql-server
